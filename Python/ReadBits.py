34d4eed9e950dc6f8882413f335a075de56b2f37
Leivio Fontenele <Leivio@yahoo.com.br>
2019-11-02 12:48:57 -0300

    +

---
+++ Python/ReadBits.py
@@ -0,0 +1,4 @@
+# Given a byte[] array, write a function byte[] readBits(int numBits) starting from LSB towards MSB. for ex, 
+# if function called to read 3 bits, read the last 3 bits from the byte and mask the remaining bits with zero. 
+# The algorithm has to be designed in such a way that we should read the bits based on the given input and save remaining 
+# bits for subsequent reads.
